10b43787d31d725409d307d0d573713e971d2df5
MZZNDR51 <MZZNDR51@eco-smat02-pc0.decentrate.univr.it>
2024-10-16 10:58:32 +0200

    Test modelli approssimanti

--- src/test/java/it/univr/trees/approximatingmodels/ApproximatingModelsTest.java
+++ src/test/java/it/univr/trees/approximatingmodels/ApproximatingModelsTest.java
@@ -20,15 +20,36 @@ public class ApproximatingModelsTest {
 		double riskFreeRate = 0.0;
 		double volatility = 0.2;
 		double lastTime = 1.0;
-		int numberOfTimes = 100;
-
-		double strike = 80;
+		int numberOfTimes = 11;
 		
-		//first test for Cox-Ross-Rubinstein: we just look at its expected value
+		//test for Cox-Ross-Rubinstein: we just look at its expected value
 		CoxRossRubinsteinModel firstApproximatingModel = new CoxRossRubinsteinModel(
 				spotPrice, riskFreeRate, volatility,  lastTime, numberOfTimes);
 		
-	
-		//da qua andremo avanti insieme
+		double[] valuesForCRR = firstApproximatingModel.getValuesAtGivenTime(lastTime);
+		
+		System.out.println("Values for Cox Ross Rubinstein:");
+		System.out.println(Arrays.toString(valuesForCRR));
+		
+		//test for Jarrow Rudd: we just look at its expected value
+		JarrowRuddModel secondApproximatingModel = new JarrowRuddModel(
+				spotPrice, riskFreeRate, volatility,  lastTime, numberOfTimes);
+		
+		double[] valuesForJR = secondApproximatingModel.getValuesAtGivenTime(lastTime);
+		
+		System.out.println("Values for Jarrow Rudd:");
+		System.out.println(Arrays.toString(valuesForJR));
+		
+		double strikeForLeisenReimer = spotPrice;
+		
+		//test for Jarrow Rudd: we just look at its expected value
+		LeisenReimerModel thirdApproximatingModel = new LeisenReimerModel(
+				spotPrice, riskFreeRate, volatility,  lastTime, numberOfTimes, strikeForLeisenReimer);
+		
+		double[] valuesForLR = thirdApproximatingModel.getValuesAtGivenTime(lastTime);
+		
+		System.out.println("Values for Leisen-Reimer:");
+		System.out.println(Arrays.toString(valuesForLR));
+		
 	}
 }
